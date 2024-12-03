6f28c044e25b72860a9ee4a4ec672d61e1b11dd1
Ali Ramazanov <toaliramazanov@gmail.com>
2024-12-03 23:08:06 +0400

    add: necessary packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 hover:text-blue-800 transition">Hello!</h1>
+    </div>
+  );
 }
 
 export default App;
